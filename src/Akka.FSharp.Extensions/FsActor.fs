03c58d3c670cd533ab1c11f8f32bbbb7a5cbd817
Ruxo Zheng <rz@just.sent.as>
2021-05-10 15:37:00 +0700

    Add FsActor for a specific message actor.

---
+++ src/Akka.FSharp.Extensions/FsActor.fs
@@ -0,0 +1,30 @@
+﻿namespace Akka.FSharp.Extensions
+
+open Akka.Actor
+
+type FsActor<'Message>() =
+    inherit UntypedActor()
+    
+    let mutable default_handler :'Message -> unit = fun _ -> ()
+
+    member _.Self = ActorBase.Context.Self
+    member _.Sender = ActorBase.Context.Sender
+
+    member _.SetDefaultHandler handler = default_handler <- handler
+
+    member this.FsBecome(handler :'Message -> unit) =
+        base.Become(UntypedReceive(this.MakeWrapper handler))
+
+    member _.FsUnbecomeStacked() = base.UnbecomeStacked()
+
+    override this.OnReceive message =
+        match message with
+        | :? 'Message as msg -> default_handler msg
+        | _ -> this.Unhandled message
+
+    member this.MakeWrapper :('Message -> unit) -> (obj -> unit) = fun handler ->
+        function
+        | :? 'Message as msg -> handler msg
+        | msg -> this.FsUnhandled msg
+
+    member _.FsUnhandled message = base.Unhandled message
--- src/Akka.FSharp.Extensions/FsReceiveActor.fs
+++ src/Akka.FSharp.Extensions/FsReceiveActor.fs
@@ -1,8 +1,18 @@
 ﻿namespace Akka.FSharp.Extensions
 
+open System
 open Akka.Actor
 
 type FsReceiveActor() =
     inherit ReceiveActor()
 
+    member _.Self = ActorBase.Context.Self
     member _.Sender = ActorBase.Context.Sender
+
+    member _.FsReceive(handler :'T -> unit) = base.Receive<'T>(handler)
+
+    member this.FsBecome(handler :unit -> unit) = base.Become(handler)
+
+    member _.FsUnbecomeStacked() = base.UnbecomeStacked()
+
+    member _.FsUnhandled message = base.Unhandled message
